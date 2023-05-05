ea893b401fa0c39835d64be8f88ce26c4b72721f
paytonholoubek <123201130+paytonholoubek@users.noreply.github.com>
2023-05-05 18:18:51 +0000

    fixed some more rooms, need to get endings done

--- Main.py
+++ Main.py
@@ -59,52 +59,52 @@ def main():
     Room5_zombiesgone = False
     Room12_zombiesgone = False
     Room19_zombiesgone = False
+    Room27_unlocked = False
 
-    controls = ("CONTROLS:\n\nWEST\nEAST\nNORTH\nSOUTH\nPICKUP\nATTACK\n")
+    controls = ("CONTROLS:\n\nWEST\nEAST\nNORTH\nSOUTH\nPICKUP\nATTACK/USE\n")
 
-    Room1 = ("You look around from the car you took to the mall, there are 4 ways you can go. Hallways to your north, west, and east. And south is the exit.")
-    Room2 = ("As you enter the room you see it used to be a cafe of some kind, you try one of the machines but it doesn't work. Dang. You decide to get back to your job and see a hallway to the west")
-    Room3 = ("You walk into a crossroads, the south is taken out so no going that way. The west leads to an apple store which you're guessing is abandoned, and north leads you to an abandoned office.")
-    Room4 = ("As you enter the apple store you see it has already been looted, the technology wouldn't have been any help since it would have been dead. To your west you see an opening to a pretty large room.")
-    Room5z = ("As you enter the doorway to your west you see a horde of zombies, with this horde a very sharp weapon would scatter them, but for now you can't go west until you scatter them")
-    Room5noz = ("You enter the doorway into a large area where you had scattered zombies earlier.")
-    Room6 = ("With the zombies scattered you continue through the large area but see nothing of interest other than a door to your north.")
-    Room7_weaponnotcollected = ("As you enter the room you don't see anything, but out of the corner of your eye you see a baseball bat in a barrel")
-    Room7 = ("When you enter the room you see nothing of value.")
-    Room8_weaponnotcollected = ("You decide to go north into the office building, and you manage to find a samurai katana. The only way you can go from here is north out the back door, our south through the way you just came.")
-    Room8 = ("You decide to go north into the office building. The only way you can go from here is north out the back door, our south through the way you just came.")
-    Room9z = ("As you leave through the backdoor, you see a disturbing sight. There is a horde of zombies just ahead of you, this is a small horde and will need a blunt object to move them.")
-    Room9noz = ("You enter a deserted hallway you scattered zombies in.")
-    Room10 = ("As you go east from your car, you see a long, uninteresting hallway that looks like it goes on for a while.")
-    Room11 = ("The long, boring hallway continues on")
-    Room12z = ("As you are walking down the long, boring hallway you eventually come upon a massive horde of zombies, you can kill them all with some a powerful weapon like a shotgun.")
-    Room12noz = ("The long, boring hallway continues on")
-    Room13 = ("Now that there aren't any zombies here, you see the boring hallway finally comes to an end in a room just east from where you are, which opens into a hunting store.")
-    Room14 = ("You don't see any bullets in the hunting store, but you see a key which looks like it could open a locked door.")
-    Room15 = ("As you enter the room from the hallway, you see trash scattered around the area, and traces of zombies. You don't see any zombies near you but it is clear they were here at one point. The building continues north")
-    Room16 = ("As you trudge through the long building, you finally come across a door. When you open it you see it opens into something into some other area to your north")
-    Room17 = ("When you open the door, you come upon a massive area, with beautiful statues now covered in moss, you guess this is the center of the mall. There are paths in all directions.")
-    Room18 = ("This hallway looks like it once was a beautiful piece of architecture, but now there is greenery covering all of it. It makes it beautiful in a weird way. The hallway continues west or east")
-    Room19 = ("Since the zombies are scattered, this area is trashed with junk thrown everywhere. This room looks like it could have been a food court in the past. There are hallways south, west, and east.")
-    Room20 = ("As you continue going west, you can hear some noises coming from a building just west from where you are.")
-    Room21_isaacnotsaved = (f"As you enter the store, you see your friend Isaac, '{player_name}! I thought you weren't gonna find me, you must've dealt with the zombies since you made it here. I am gonna go to the car and place my supplies there, see you there!' With Isaac leaving, you see nothing else in this room.")
-    Room21_isaacsaved = ("As you enter the room, you see some stray rats scrambling around, how did these rats cause all this ruckus?")
-    Room22_weaponnotcollected = ("You decide to go east, and as you are walking across the hallway, you see a shotgun to the right. The hallway continues on East")
-    Room22 = ("You decide to go east as the hallway continues")
-    Room23 = ("You enter a weird art store, there aren't any paintings that catch your attention, the art room continues east")
-    Room24_normal = ("You continue going down the art store until you come to a dead end, with a picture of a man smiling at you, he looks friendly.")
-    Room24_trueending = ("You continue going down the art store until you come to the end, where you see a picture of a man smiling at you, he looks friendly.")
-    Room25 = ("You decide to go north and come up to an escalator. Well it used to be, now since there isn't power it is basically just a staircase. It continues north.")
-    Room26 = ("As you ascend the stairs, they seemed to go on forever. Once you reach the top, to your north you see a locked door. You see that it needs a key to be opened")
-    Room27_nokey = ("You can't continue until you find a way through the locked door")
-    Room27 = ("As you open the door you come across a beautiful room, untouched by nature with hallways north, east, and west. The north seems to head to a boys bathroom, and the east is a girls bathroom.")
-    Room28_weaponnotcollected = ("As you enter the room, you realise this is a police department for the mall, you see a baton hanging up on the wall")
-    Room28_weaponcollected = ("When you enter the room, you realise this is a police department for the mall.")
-    Room29 = ("When you enter the girls bathroom, the only thing there remotely close to a girl are the rats running around.")
-    Room30_tazznotsaved = (f"As you enter the bathroom, you hear a massive fart and plop. 'Is that you Tazz?' you call out. All the sudden a stall opens and you see Tazz come out of it. '{player_name}! You got me at a bad time but I was stuck here and needed to do my thing yknow. I actually gathered some food for the colony, I will meet you at the car!'")
-    Room30_tazzsaved = ("You enter the boys bathroom and hear a disfunctional toilet constantly flushing, how is that thing still working?")
-    Zombies_in_room = ("You can't go this way with zombies right there!")
-    Locked_door_room = ("You can't continue until you find a way through this door!")
+    Room1 = ("\nYou look around from the car you took to the mall, there are 4 ways you can go. Hallways to your north, west, and east. And south is the exit.")
+    Room2 = ("\nAs you enter the room you see it used to be a cafe of some kind, you try one of the machines but it doesn't work. Dang. You decide to get back to your job and see a hallway to the west")
+    Room3 = ("\nYou walk into a crossroads, the south is taken out so no going that way. The west leads to an apple store which you're guessing is abandoned, and north leads you to an abandoned office.")
+    Room4 = ("\nAs you enter the apple store you see it has already been looted, the technology wouldn't have been any help since it would have been dead. To your west you see an opening to a pretty large room.")
+    Room5z = ("\nAs you enter the doorway to your west you see a horde of zombies, with this horde a very sharp weapon would scatter them, but for now you can't go west until you scatter them")
+    Room5noz = ("\nYou enter the doorway into a large area where you had scattered zombies earlier.")
+    Room6 = ("\nWith the zombies scattered you continue through the large area but see nothing of interest other than a door to your north.")
+    Room7_weaponnotcollected = ("\nAs you enter the room you don't see anything, but out of the corner of your eye you see a baseball bat in a barrel")
+    Room7 = ("\nWhen you enter the room you see nothing of value.")
+    Room8_weaponnotcollected = ("\nYou decide to go north into the office building, and you manage to find a samurai katana. The only way you can go from here is north out the back door, our south through the way you just came.")
+    Room8 = ("\nYou decide to go north into the office building. The only way you can go from here is north out the back door, our south through the way you just came.")
+    Room9z = ("\nAs you leave through the backdoor, you see a disturbing sight. There is a horde of zombies just ahead of you, this is a small horde and will need a blunt object to move them.")
+    Room9noz = ("\nYou enter a deserted hallway you scattered zombies in.")
+    Room10 = ("\nAs you go east from your car, you see a long, uninteresting hallway that looks like it goes on for a while.")
+    Room11 = ("\nThe long, boring hallway continues on")
+    Room12z = ("\nAs you are walking down the long, boring hallway you eventually come upon a massive horde of zombies, you can kill them all with some a powerful weapon like a shotgun.")
+    Room12noz = ("\nThe long, boring hallway continues on")
+    Room13 = ("\nNow that there aren't any zombies here, you see the boring hallway finally comes to an end in a room just east from where you are, which opens into a hunting store.")
+    Room14 = ("\nYou don't see any bullets in the hunting store, but you see a key which looks like it could open a locked door.")
+    Room15 = ("\nAs you enter the room from the hallway, you see trash scattered around the area, and traces of zombies. You don't see any zombies near you but it is clear they were here at one point. The building continues north")
+    Room16 = ("\nAs you trudge through the long building, you finally come across a door. When you open it you see it opens into something into some other area to your north")
+    Room17 = ("\nWhen you open the door, you come upon a massive area, with beautiful statues now covered in moss, you guess this is the center of the mall. There are paths in all directions.")
+    Room18 = ("\nThis hallway looks like it once was a beautiful piece of architecture, but now there is greenery covering all of it. It makes it beautiful in a weird way. The hallway continues west or east")
+    Room19 = ("\nSince the zombies are scattered, this area is trashed with junk thrown everywhere. This room looks like it could have been a food court in the past. There are hallways south, west, and east.")
+    Room20 = ("\nAs you continue going west, you can hear some noises coming from a building just west from where you are.")
+    Room21_isaacnotsaved = (f"\nAs you enter the store, you see your friend Isaac, '{player_name}! I thought you weren't gonna find me, you must've dealt with the zombies since you made it here. I am gonna go to the car and place my supplies there, see you there!' With Isaac leaving, you see nothing else in this room.")
+    Room21_isaacsaved = ("\nAs you enter the room, you see some stray rats scrambling around, how did these rats cause all this ruckus?")
+    Room22_weaponnotcollected = ("\nYou decide to go east, and as you are walking across the hallway, you see a shotgun to the right. The hallway continues on East")
+    Room22 = ("\nYou decide to go east as the hallway continues")
+    Room23 = ("\nYou enter a weird art store, there aren't any paintings that catch your attention, the art room continues east")
+    Room24_normal = ("\nYou continue going down the art store until you come to a dead end, with a picture of a man smiling at you, he looks friendly.")
+    Room24_trueending = ("\nYou continue going down the art store until you come to the end, where you see a picture of a man smiling at you, he looks friendly.")
+    Room25 = ("\nYou decide to go north and come up to an escalator. Well it used to be, now since there isn't power it is basically just a staircase. It continues north.")
+    Room26 = ("\nAs you ascend the stairs, they seemed to go on forever. Once you reach the top, to your north you see a locked door. You see that it needs a key to be opened")
+    Room27_nokey = ("\nYou can't continue until you find a way through the locked door")
+    Room27 = ("\nAs you open the door you come across a beautiful room, untouched by nature with hallways north, east, and west. The north seems to head to a boys bathroom, and the east is a girls bathroom.")
+    Room28 = ("\nWhen you enter the room, you realise this is a police department for the mall. You don't find anything of value.")
+    Room29 = ("\nWhen you enter the girls bathroom, the only thing there remotely close to a girl are the rats running around.")
+    Room30_tazznotsaved = (f"\nAs you enter the bathroom, you hear a massive fart and plop. 'Is that you Tazz?' you call out. All the sudden a stall opens and you see Tazz come out of it. '{player_name}! You got me at a bad time but I was stuck here and needed to do my thing yknow. I actually gathered some food for the colony, I will meet you at the car!'")
+    Room30_tazzsaved = ("\nYou enter the boys bathroom and hear a disfunctional toilet constantly flushing, how is that thing still working?")
+    Zombies_in_room = ("\nYou can't go this way with zombies right there!")
+    Locked_door_room = ("\nYou can't continue until you find a way through this door!")
 #GAME START
 
     print("After some Chinese scientist had a failed experiment, zombies began to appear all throughout the world and cause a meltdown of humanity")
@@ -129,10 +129,27 @@ def main():
 
     while True:
         player_input = ""
-        player_input = input("What do you want to do?\n").lower()
+        player_input = input("\nWhat do you want to do?\n").lower()
 
-        if player_input == "break":
+        if player_input == "quit":
               break
+        
+        if player_input == "controls":
+              print(controls)
+        
+        if player_input == "command.giveall":
+              inventory.append(item1)
+              katanaPickedUp = True
+              inventory.append(item2)
+              baseballbatPickedUp = True
+              inventory.append(item3)
+              shotgunPickedUp = True
+              inventory.append(item4)
+              keyPickedUp = True
+              print("Given every weapoon in the game.")
+
+      
+
 
         if "room" + str(room_number) == "room1": #Room 1
                 if player_input == "north":
@@ -612,7 +629,7 @@ def main():
                     print("You cannot go this way")
                 if player_input == "west":
                       room_number = 23
-                      print(Room4)
+                      print(Room23)
                 if player_input == "east":
                       #room_number = 0
                       print("You cannot go this way.")
@@ -644,10 +661,10 @@ def main():
 
         elif "room" + str(room_number) == "room26": #Room 26
                 if player_input == "north":
-                    room_number = 27
-                    if keyPickedUp == False: #only lets the player in the locked door if they have the key
+                    if Room27_unlocked == False: #only lets the player in the locked door if they have the key
                           print(Room27_nokey)
-                    elif keyPickedUp == True:
+                    elif Room27_unlocked == True:
+                          room_number = 27
                           print(Room27)
                 if player_input == "west":
                       #room_number = 0
@@ -661,7 +678,12 @@ def main():
                 if player_input == "pickup":
                       print("you cannot pick anything up here.")
                 if player_input == "attack":
-                      print("you cannot attack here")
+                      print("You decide to use logic and try to kick the door down. Since it is rusted you take it down and continue on.")
+                      Room27_unlocked = True
+                if player_input == "use" and item4 in inventory:
+                      print("You use the key and successfully unlock the door allowing you to continue.")
+                      Room27_unlocked = True
+                  
 
         elif "room" + str(room_number) == "room27": #Room 27
                 if player_input == "north":
@@ -672,13 +694,10 @@ def main():
                           print(Room30_tazzsaved)
                 if player_input == "west":
                       room_number = 28
-                      if batonPickedUp == False:
-                            print(Room28_weaponnotcollected)
-                      elif batonPickedUp == True:
-                            print(Room28_weaponcollected)
+                      print(Room28)
                 if player_input == "east":
-                      room_number = 4
-                      print(Room2)
+                      room_number = 29
+                      print(Room29)
                 if player_input == "south":
                     #room_number = 0
                     print("you cannot go this way.")
